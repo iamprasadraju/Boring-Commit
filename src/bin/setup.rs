d3330fcba9f39432687e98cbbc10725101266379
iamprasadraju <gls.prasadraju@gmail.com>
2026-09-18 22:26:56 +0530

    add default toml config file and some improvements in setup.rs

--- src/bin/setup.rs
+++ src/bin/setup.rs
@@ -1,72 +1,67 @@
-use std::process::Command;
-use std::env;
+use dirs;
 use std::fs;
 use std::path::PathBuf;
+use std::process::Command;
 
 /*pub fn setup(){
-    
-        
+
+
 }
 */
 
-pub fn ollama(){
+// read this file at compile time and turn its contents into string
+const DEFAULT_CONFIG: &str = include_str!("../config/default.toml");
+
+pub fn ollama() {
     println!("Boring Commit setup => ");
-        if cfg!(target_os = "windows"){
-            Command::new("powershell")
-                .args(["-Command", "irm https://ollama.com/install.ps1 | iex"])
-                .status()
-                .expect("failed to install Ollama");
-        }
-        else {
-            Command::new("sh")
-                .arg("-c")
-                .arg("curl -fsSL https://ollama.com/install.sh | sh")
-                .status()
-                .expect("failed to install Ollama");
-        }
+    if cfg!(target_os = "windows") {
+        Command::new("powershell")
+            .args(["-Command", "irm https://ollama.com/install.ps1 | iex"])
+            .status()
+            .expect("failed to install Ollama");
+    } else {
+        Command::new("sh")
+            .arg("-c")
+            .arg("curl -fsSL https://ollama.com/install.sh | sh")
+            .status()
+            .expect("failed to install Ollama");
+    }
 }
 
 pub fn config_file_path() -> PathBuf {
-    
-    // macOS/linux: ~/.config/bcommit/config.toml
-    // Windows: C:\Users\<YourUsername>\AppData\Roaming\bcommit\config.toml
-    
-    let home_dir = env::home_dir()
-    .expect("could not find home directory");
+    //Linux: ~/.config/bcommit/config.toml
+    // macOS: ~/Library/Application Support/bcommit/config.toml
+    // Windows: %APPDATA%\bcommit\config.toml
 
-    let config_dir = if cfg!(target_os = "windows") {
-        home_dir
-            .join("AppData")
-            .join("Roaming")
-            .join("bcommit")   
-    }else {
-        home_dir
-            .join(".config")
-            .join("bcommit")
-    };
+    let config_dir = dirs::config_dir()
+        .expect("could not find config directory")
+        .join("bcommit");
 
-    fs::create_dir_all(&config_dir)
-        .expect("failed to create config dir");
-    
-    config_dir.join("config.toml")    
+    fs::create_dir_all(&config_dir).expect("failed to create config directory");
+
+    config_dir.join("config.toml")
 }
 
-pub fn config_file_create(){
-    let boring_line = "# Boring Commit Config";
-    fs::write(&config_file_path(), boring_line)
-            .expect("failed to create config file");
+pub fn config_file_create() {
+    let path = config_file_path();
+
+    if path.exists() {
+        return;
+    }
+
+    fs::write(path, DEFAULT_CONFIG).expect("failed to create config file")
 }
 
-pub fn config_file_read() -> String{
-    fs::read_to_string(&config_file_path())
-        .expect("unable to read the file")
+pub fn config_file_read() -> String {
+    let path = config_file_path();
+
+    fs::read_to_string(path).expect("unable to read the file")
 }
 
-fn main(){
+fn main() {
     // let path = config_file_path();
-    // config_file_create();
-    
+    config_file_create();
+
     let con = config_file_read();
-    
-    println!("{:?}", con);
+    println!("{}", con);
 }
